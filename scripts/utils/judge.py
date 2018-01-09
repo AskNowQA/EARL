0c475151f5f286ea0602aa6d1117e2e839b91fd0
Debayan Banerjee <debayanin@gmail.com>
2018-01-09 20:13:05 +0100

    Fixing judge script

--- scripts/utils/judge.py
+++ scripts/utils/judge.py
@@ -30,15 +30,18 @@ for item in d:
     for ent in item['entities']:
          for uri in ent['uris']:
              itarr.append(uri['uri'])
+             break
     for rel in item['relations']:
          for uri in rel['uris']:
              itarr.append(uri['uri'])
+             break
     earltest.append(itarr)
 
 correct = 0
 wrong = 0
 total = 0
 for idx,arr in enumerate(lcqgold):
+    print(lcqgold[idx],earltest[idx])
     for uri in arr:
         if '/resource/' in uri:
             if uri in earltest[idx]:
