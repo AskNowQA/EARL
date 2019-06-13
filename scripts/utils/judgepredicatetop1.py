a97f8f65351fb778a1990e22c559cde22033779a
Debayan Banerjee <debayanin@gmail.com>
2019-06-13 16:47:58 +0200

    upper and lower case parse and judge scripts

---
+++ scripts/utils/judgepredicatetop1.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+
+from __future__ import print_function
+import sys,json,urllib, urllib2, requests
+import requests
+
+result = []
+count = 0
+f = open(sys.argv[1])
+s = f.read()
+d = json.loads(s)
+f.close()
+lcqgold = []
+earltest = []
+
+for item in d: 
+    itarr = []
+    for ent in item['entity mapping']:
+        itarr.append(ent['uri'])
+    for pred in item['predicate mapping']:
+        itarr.append(pred['uri'])
+    lcqgold.append(itarr)
+
+f = open(sys.argv[2])
+s = f.read()
+d = json.loads(s)
+f.close()
+for items in d:
+    itarr = []
+    for item in items:
+        for k,v in item['rerankedlists'].iteritems():
+            itarr.append(v[0][1])
+        break
+    earltest.append(itarr)
+
+correct = 0
+wrong = 0
+total = 0
+for idx,arr in enumerate(lcqgold):
+    print(lcqgold[idx],earltest[idx])
+    for uri in arr:
+        if  '/ontology/' in uri:
+            if uri in earltest[idx]:
+                correct += 1
+            else:
+                wrong += 1
+            total += 1
+print(total)
+print(correct/float(total))
+
---
+++ scripts/utils/parseearlupper.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+from __future__ import print_function
+import sys,json,urllib, urllib2, requests
+import requests
+
+f1 = open('outputerspanupper.json','w')
+
+result = []
+count = 0
+f = open(sys.argv[1])
+s = f.read()
+d = json.loads(s)
+f.close()
+for item in d: 
+    req = urllib2.Request('http://localhost:4444/processQuery')
+    req.add_header('Content-Type', 'application/json')
+    inputjson = {'nlquery': item['question']}
+    response = urllib2.urlopen(req, json.dumps(inputjson))
+    response = json.loads(response.read())
+    #j = json.dumps(response, indent=4)
+    print(count, len(d))
+    result.append(response)
+    count += 1
+print(json.dumps(result),file=f1)
+f1.close()
+
