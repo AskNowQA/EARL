71b2450b0516ec3aba9c1ad6f7470e6f752eb259
Debayan Banerjee <debayanin@gmail.com>
2019-06-19 00:28:22 +0200

    multi threaded url hit

--- scripts/utils/parseearlupper.py
+++ scripts/utils/parseearlupper.py
@@ -3,25 +3,43 @@
 from __future__ import print_function
 import sys,json,urllib, urllib2, requests
 import requests
+from multiprocessing import Pool
 
-f1 = open('outputerspanupperfulllcquad.json','w')
+def hiturl(question):
+    serialnum = question[1]
+    question = question[0]
+    req = urllib2.Request('http://localhost:4440/processQuery')
+    req.add_header('Content-Type', 'application/json')
+    inputjson = {'nlquery': question}
+    response = urllib2.urlopen(req, json.dumps(inputjson))
+    responsestring = response.read()
+    if responsestring is None:
+        return (int(serialnum),'{}')
+    else:
+        return (int(serialnum),responsestring)
 
-result = []
+
 count = 0
 f = open(sys.argv[1])
 s = f.read()
 d = json.loads(s)
 f.close()
-for item in d: 
-    req = urllib2.Request('http://localhost:4444/processQuery')
-    req.add_header('Content-Type', 'application/json')
-    inputjson = {'nlquery': item['question']}
-    response = urllib2.urlopen(req, json.dumps(inputjson))
-    response = json.loads(response.read())
-    #j = json.dumps(response, indent=4)
-    print(count, len(d))
-    result.append(response)
-    count += 1
-print(json.dumps(result),file=f1)
+questions = []
+
+for item in d:
+    questions.append((item['question'],item['SerialNumber']))
+
+pool = Pool(8)
+responses = pool.map(hiturl,questions)
+
+responses = sorted(responses, key=lambda tup: tup[0])
+
+results = []
+
+for response in responses:
+    results.append(json.loads(response[1]))
+ 
+f1 = open('outputerspanupperfulllcquad.json','w')
+print(json.dumps(results),file=f1)
 f1.close()
 
