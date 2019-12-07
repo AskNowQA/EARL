f57134c316e61ef60cd155589199597c98029ed9
Debayan Banerjee <debayanin@gmail.com>
2019-12-07 21:53:10 +0000

    Add reranker trained on all 3 datasets

--- scripts/ReRanker.py
+++ scripts/ReRanker.py
@@ -3,8 +3,6 @@
 import numpy as np
 from elasticsearch import Elasticsearch
 import urllib2,copy,json,sys,torch
-from annoy import AnnoyIndex
-
 device = torch.device('cuda')
 
 class ReRanker:
@@ -12,7 +10,8 @@ class ReRanker:
         self.es = Elasticsearch()
         print "ReRanker initializing"
         try:
-            N, D_in, H1, H2, H3, H4, D_out = 100, 702, 500, 300, 100, 10, 1
+            N, D_in, H1, H2, H3 ,H4, D_out = 200, 501, 300, 150, 70, 30, 1
+
             self.model = torch.nn.Sequential(
               torch.nn.Linear(D_in, H1),
               torch.nn.ReLU(),
@@ -24,11 +23,8 @@ class ReRanker:
               torch.nn.ReLU(),
               torch.nn.Linear(H4, D_out)
             ).to(device)
-            self.model.load_state_dict(torch.load('../data/jointreranker.122966.model'))
-            self.model.eval() 
-            self.t = AnnoyIndex(200,'dot')
-            self.t.load('../data/wikiembedrel1.ann')
-            self.treldict = json.loads(open('../data/wikiemberrel1dict.json').read())
+            self.model.load_state_dict(torch.load('../data/jointreranker.049573.model'))
+            self.model.eval()
         except Exception,e:
             print e
             sys.exit(1)
@@ -53,67 +49,20 @@ class ReRanker:
             return None
         return None
 
-    def getrelembedding(self, entityurl):
-        res = es.search(index="wikidataembedsindex01", body={"query":{"term":{"key":{"value":entityurl}}}})
-        try:
-            embedding = [float(x) for x in res['hits']['hits'][0]['_source']['embedding']]
-            return embedding
-        except Exception as e:
-            print(e)
-            return None
-        return None
-    
-    def getjointembedding(self, chunkembeddings, chunkindex, embedding):
-        bestdot = -1
-        bestrelurl = None
-        bestrelembedding = None
-        for qchunkindex,qentities in chunkembeddings.items():
-            if chunkindex == qchunkindex:
-                continue
-            for qentid,qembedding in qentities.items():
-                queryrel = np.asarray(qembedding) - np.asarray(embedding)
-                matchedrels = self.t.get_nns_by_vector(queryrel, 10, include_distances = True)
-                if matchedrels[1][0] > bestdot:
-                    bestdot = matchedrels[1][0]
-                    bestrelurl = self.treldict[str(matchedrels[0][0])]
-                    bestrelembedding = self.getrelembedding(treldict[str(matchedrels[0][0])])
-                matchedrels = self.t.get_nns_by_vector(np.negative(queryrel), 10, include_distances = True)
-                if matchedrels[1][0] > bestdot:
-                    bestdot = matchedrels[1][0]
-                    bestrelurl = self.treldict[str(matchedrels[0][0])]
-                    bestrelembedding = self.getrelembedding(treldict[str(matchedrels[0][0])])
-        if not bestrelembedding:
-            return embedding + [-1]*200 + [bestdot]
-        else:
-            return embedding + bestrelembedding + [bestdot]
-
-
     def rerank(self, topklists, nlquery):
         rerankedlists = []
         questionembedding = self.getsentenceembedding(nlquery)
-        chunkembeddings = {}
-        listoflistofentities = []
-        for chunkindex,chunk in enumerate(topklists):
-            if chunkindex not in chunkembeddings:
-                chunkembeddings[chunkindex] = {}
+        for chunk in topklists:
+            inputfeatures = []
             for idx,entid in enumerate(chunk['topkmatches']):
-                embedding = self.getentityembedding(entid)
-                if not embedding:
-                    print("no embedding for %s"%entid)
+                entityembedding = self.getentityembedding(entid)
+                if not entityembedding or not questionembedding:
                     continue
-                chunkembeddings[chunkindex][entid] = embedding
-            listoflistofentities.append(topklists[chunkindex]['topkmatches'])
-        inputfeatures = []
-        entities = []
-        for chunkidx,listofentities in enumerate(listoflistofentities):
-            for entidx,entid in enumerate(listofentities):
-                entembedding = chunkembeddings[chunkidx][entid]
-                vector = self.getjointembedding(chunkembeddings,chunkidx,entembedding)
-                inputfeatures.append(vector+[entidx]+questionembedding)
-                entities.append(entid)
+                vector = [idx]+entityembedding+questionembedding
+                inputfeatures.append(vector)
             x = torch.FloatTensor(inputfeatures).to(device)
             preds = self.model(x).to(device).reshape(-1).cpu().detach().numpy()
-            l = [(float(p),u) for p,u in zip(preds, entities)]
+            l = [(float(p),u) for p,u in zip(preds, chunk['topkmatches'])]
             reranked = sorted(l, key=lambda x: x[0], reverse=True)
             if reranked[0][0] < 0.5:
                 continue
