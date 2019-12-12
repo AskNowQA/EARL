756520f4452dbd00200edb0042b0b1598debd7d4
Debayan Banerjee <debayanin@gmail.com>
2019-12-12 14:30:06 +0000

    Adding modified training data generation scripts, with entity spans removed from question before claculating sentence embeddings

--- scripts/utils/embedtrain/generatetrainingdata.py
+++ scripts/utils/embedtrain/generatetrainingdata.py
@@ -1,4 +1,4 @@
-import sys,os,json,re,urllib2
+import sys,os,json,re,urllib2,random
 from elasticsearch import Elasticsearch
 import torch
 
@@ -28,39 +28,45 @@ for gold,query in zip(dlcqtrain,dquerytrain):
         query = query[1]
         if len(query) == 0:
             continue
-        goldents = re.findall( r'wd:(.*?) ', gold['sparql_wikidata'])
+        goldents = re.findall( r'wd:([Q][0-9]*)', gold['sparql_wikidata'])
         question = gold['question']
+        for chunk in query:
+            question = question.replace(chunk['chunk']['chunk'], "")
+        question = re.sub(r"[^a-zA-Z0-9]+", ' ', question)
         req = urllib2.Request('http://localhost:8887/ftwv')
         req.add_header('Content-Type', 'application/json')
         inputjson = {'chunks':[question]}
         response = urllib2.urlopen(req, json.dumps(inputjson))
         embedding = json.loads(response.read().decode('utf8'))[0]
         questionembedding = embedding
-        paraphrased_question = gold['paraphrased_question']
-        req = urllib2.Request('http://localhost:8887/ftwv')
-        req.add_header('Content-Type', 'application/json')
-        inputjson = {'chunks':[paraphrased_question]}
-        response = urllib2.urlopen(req, json.dumps(inputjson))
-        embedding = json.loads(response.read().decode('utf8'))[0]
-        paraphrased_questionembedding = embedding
+        #paraphrased_question = gold['paraphrased_question']
+        #req = urllib2.Request('http://localhost:8887/ftwv')
+        #req.add_header('Content-Type', 'application/json')
+        #inputjson = {'chunks':[paraphrased_question]}
+        #response = urllib2.urlopen(req, json.dumps(inputjson))
+        #embedding = json.loads(response.read().decode('utf8'))[0]
+        #paraphrased_questionembedding = embedding
+        true = []
+        false = []
         for chunk in query:
             for idx,entid in enumerate(chunk['topkmatches']):
                 embedding = getembedding(entid)
                 if embedding:
                     if entid in goldents:
-                        if len(question) > 0 and len(question) < 100:
-                            trainingdata.append([entid,idx,embedding,question,questionembedding,1.0])
-                        if len(paraphrased_question) > 0 and len(paraphrased_question) < 100:
-                            trainingdata.append([entid,idx,embedding,paraphrased_question,paraphrased_questionembedding,1.0])
+                        if len(question) > 0 and len(question) < 200:
+                            true.append([entid,idx,embedding,question,questionembedding,1.0])
+                        #if len(paraphrased_question) > 0 and len(paraphrased_question) < 200:
+                        #    trainingdata.append([entid,idx,embedding,paraphrased_question,paraphrased_questionembedding,1.0])
                     else:
-                        if len(question) > 0 and len(question) < 100:
-                            trainingdata.append([entid,idx,embedding,question,questionembedding,0.0])
-                        if len(paraphrased_question) > 0 and len(paraphrased_question) < 100:
-                            trainingdata.append([entid,idx,embedding,paraphrased_question,paraphrased_questionembedding,0.0])
+                        if len(question) > 0 and len(question) < 200:
+                            false.append([entid,idx,embedding,question,questionembedding,0.0])
+                        #if len(paraphrased_question) > 0 and len(paraphrased_question) < 200:
+                        #    trainingdata.append([entid,idx,embedding,paraphrased_question,paraphrased_questionembedding,0.0])
+        trainingdata += true
+        trainingdata += [random.choice(false) for i in range(len(true)) ] 
     except Exception as e:
         print(e)
 
 f = open('embedsimpletrainvectors1.json','w')
 f.write(json.dumps(trainingdata))
 f.close()
-             
