afdc95dfa96c93c9244aa18310dbe18bd4c92e07
Debayan Banerjee <debayanin@gmail.com>
2020-03-22 00:54:02 +0000

    nif  compatible api

--- scripts/api.py
+++ scripts/api.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 from flask import request
+from flask import Response
 from flask import Flask
 from gevent.pywsgi import WSGIServer
 import json,sys,requests,logging
@@ -9,6 +10,8 @@ from  TextMatch import TextMatch
 #from JointLinker import JointLinker
 from ReRanker import ReRanker
 import json
+from pynif import NIFCollection
+
 logging.basicConfig(filename='/var/log/asknow/earl.log',level=logging.INFO)
 e = ERSpanDetector()
 t = TextMatch()
@@ -19,7 +22,6 @@ reload(sys)
 sys.setdefaultencoding('utf8')
 
 app = Flask(__name__)
-
 
 
 @app.route('/processQuery', methods=['POST'])
@@ -42,6 +44,32 @@ def processQuery():
     reranked = r.rerank(topkmatches, nlquery)
     print "ReRanked lists: %s"%json.dumps(reranked)
     return json.dumps(reranked, indent=4, sort_keys=True)
+
+@app.route('/nif', methods=['GET','POST'])
+def processQueryNif():
+    content_format = request.headers.get('Content') or 'application/x-turtle'
+    nif_body = request.data.decode("utf-8")
+    print(nif_body)
+    try:
+        nif_doc = NIFCollection.loads(nif_body, format='turtle')
+        #print(nif_doc)
+        for context in nif_doc.contexts:
+            erprediction = e.erspan(context.mention)[0]
+            print "ER Predictions: %s"%json.dumps(erprediction)
+            topkmatches = t.textMatch(erprediction, False)
+            print "Top text matches: %s"%json.dumps(topkmatches)
+            reranked = r.rerank(topkmatches, context.mention)
+            print "ReRanked lists: %s"%json.dumps(reranked)
+            for chunk in reranked:
+                context.add_phrase(beginIndex=0, endIndex=1, taIdentRef='http://www.wikidata.org/entity/'+chunk['reranked'][0][0])
+        resp = Response(nif_doc.dumps())
+        print(nif_doc.dumps())
+        resp.headers['content-type'] = content_format
+        return resp
+    except Exception as err:
+        print(err)
+        return ''
+    return ''
 
 if __name__ == '__main__':
     http_server = WSGIServer(('', int(sys.argv[1])), app)
