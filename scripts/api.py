0722d98ef0f1933017da41407f4036a97dfec20b
Debayan Banerjee <debayanin@gmail.com>
2020-03-22 00:32:52 +0000

    nif web api works

--- scripts/api.py
+++ scripts/api.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python
 
-from flask import request
+from flask import request, Response
 from flask import Flask
 from gevent.pywsgi import WSGIServer
 import json,sys,requests,logging
 import json
 from Vectoriser import Vectoriser
 from PointerNetworkLinker import PointerNetworkLinker
+from pynif import NIFCollection
+from rdflib import URIRef
+
 logging.basicConfig(filename='/var/log/asknow/earl.log',level=logging.INFO)
 v = Vectoriser()
 p = PointerNetworkLinker()
@@ -31,6 +34,34 @@ def processQuery():
     print("Vectorisation length %d"%(len(vectors)))
     entities = p.link(vectors)
     return json.dumps({'nlquery':d['nlquery'],'entities':entities}, indent=4, sort_keys=True)
+
+@app.route('/nif', methods=['GET','POST'])
+def processQueryNif():
+    print("inside")
+    content_format = request.headers.get('Content') or 'application/x-turtle'
+    nif_body = request.data.decode("utf-8")
+    print(nif_body)
+    try:
+        nif_doc = NIFCollection.loads(nif_body, format='turtle')
+        #print(nif_doc)
+        for context in nif_doc.contexts:
+            vectors = v.vectorise(context.mention)
+            entities = p.link(vectors)
+            s = set()
+            for idx,entityarr in entities.items():
+                for ent in entityarr:
+                    s.add(ent[0])
+            for entity in s:
+                context.add_phrase(beginIndex=0, endIndex=1, taIdentRef='http://www.wikidata.org/entity/'+entity)
+        resp = Response(nif_doc.dumps())
+        print(nif_doc.dumps())
+        resp.headers['content-type'] = content_format
+        return resp
+    except Exception as e:
+        print(e)
+        return ''
+    return ''
+
 
 if __name__ == '__main__':
     http_server = WSGIServer(('', int(sys.argv[1])), app)
